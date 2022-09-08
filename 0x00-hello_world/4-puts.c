40f1be5331d2e657102f6b5ba53c5b558250fc13
akadwa <kadazh123@gmail.com>
2022-09-08 08:09:01 -0700

    A function to print out a string using "puts"; file 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Description: 'Print out the given string using command puts.'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[50] = "\"Programming is like building a multilingual puzzle"\n;
+
+	puts(str);
+
+	return (0);
+}
