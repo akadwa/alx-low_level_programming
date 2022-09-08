618a3bdf0d35627c84ae134c8ef673d6f5395273
akadwa <kadazh123@gmail.com>
2022-09-08 08:24:40 -0700

    A function to print out a string using "printf"; file 5.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'Print a string using the printf command.'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
