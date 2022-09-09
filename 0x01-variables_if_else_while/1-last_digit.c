38a78e1820a863d4e1e583d3b018ca109da1c3c4
akadwa <kadazh123@gmail.com>
2022-09-09 07:03:56 -0700

    A program which will assign a random number to the variable n, and then print out a suitable message each time it is executed; file 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,19 +11,20 @@
 int main(void)
 {
 		int n;
+		int last = n % 10;
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 
-		if ((n % 10) > 5)
+		if ((last) > 5)
 		{
-			printf("Last digit of %d is \(%n\%\10\) and is greater than 5\n", n);
+			printf("Last digit of %d is %d and is greater than 5\n", n, last);
 		} else if ((n % 10) == 0)
 		{
-			printf("Last digit of %d is \(%n\%\10\) and is 0\n", n);
+			printf("Last digit of %d is %d and is 0\n", n, last);
 		} else
 		{
-			printf("Last digit of %d is \(%n\%\10\) and is less than 6 and not 0\n", n);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 		}
 
 		return (0);
