f2bc8c395b489265a6ab08de1fdf642f90a36c22
akadwa <kadazh123@gmail.com>
2022-09-09 06:10:02 -0700

    A program which will assign a random number to the variable n, and then print out a suitable message each time it is executed; file 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there
-* main - entry point
-* Description - 'Program will assign a random number to the variable n each time it is executed, and will print out a reult depending on n.'
-* betty style doc for function main goes there
+#include <stdio.h>
+
+/**
+*main - entry point
+* Description - 'Assign random number to n and print message when executed'
+* Return: Always 0 (Success)
 */
 int main(void)
 {
@@ -12,11 +14,14 @@ int main(void)
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 		/* your code goes there */
-		if (n > 0) {
+		if (n > 0)
+		{
 			printf("%d is positive\n", n);
-		} else if (n == 0) {
+		} else if (n == 0)
+		{
 			printf("%d is zero\n", n);
-		} else {
+		} else
+		{
 			printf("%d is negative\n", n);
 		}
 
