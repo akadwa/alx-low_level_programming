022468393ad2179f072b410025b8a9083ce2760f
akadwa <kadazh123@gmail.com>
2022-09-13 13:00:22 -0700

    Removed trailing comma

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,17 +10,18 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (n = n; n <= 98; n++)
+		for (n = n; n < 98; n++)
 		{
 			printf("%d", n);
 			printf(", ");
 		}
 	} else
 	{
-		for (n = n; n >= 98; n--)
+		for (n = n; n > 98; n--)
 		{
 			printf("%d", n);
 			printf(", ");
 		}
+	printf("98");
 	}
 }
