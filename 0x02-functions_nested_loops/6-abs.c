a1624a99ea1856670ee3169e99d95358fe972254
akadwa <kadazh123@gmail.com>
2022-09-13 07:40:01 -0700

    Added file 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Gives the absolute value of an integer
+ * @int : the integer to be converted
+ * Return: Always 0(success)
+ */
+
+int _abs(int)
+{
+	int abs(int);
+
+	return (0);
+}
