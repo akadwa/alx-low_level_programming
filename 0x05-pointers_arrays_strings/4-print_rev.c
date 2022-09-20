155f3cf265a4b983a5a6ab4a5ecada657fe410e7
akadwa <kadazh123@gmail.com>
2022-09-20 09:22:43 -0700

    Removed line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,15 +11,14 @@ void print_rev(char *s)
 	int length = 0;
 	int i;
 
-	while (*s != '\0')
+	while (s[length] != '\0')
 	{
 		length++;
-		s++;
 	}
 
 	for (i = length - 1; i >= 0; i--)
 	{
-		_putchar(*s);
+		_putchar(s[c]);
 	}
 	_putchar('\n');
 }
