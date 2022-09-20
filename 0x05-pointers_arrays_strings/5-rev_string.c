555d520225ad842d3f04da09ce90b63e68f2beda
akadwa <kadazh123@gmail.com>
2022-09-20 10:24:31 -0700

    Added file 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ *
+ * @s : pointer to variable of type character
+ */
+
+void rev_string(char *s)
+{
+	int length, c;
+	char *begin, *end, temp;
+
+	length = _strlen(s);
+	begin = s;
+	end = s;
+
+	for (c = 0; c < length - 1; c++)
+		end++;
+
+	for (c = 0; c < length / 2; c++)
+	{
+		temp = *end;
+		*end = *begin;
+		*begin = temp;
+
+		begin++;
+		end--;
+	}
+}
