58568a5dc08b2a48ea04a48be578ccb23b4370f9
akadwa <kadazh123@gmail.com>
2022-09-22 08:10:18 -0700

    Added pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,18 +13,18 @@ int _strcmp(char *s1, char *s2)
 {
 	int count;
 
-	while (s1 == s2)
+	while (*s1 == *s2)
 	{
-		if (s1 == '\0' || s2 == '\0')
+		if (*s1 == '\0' || *s2 == '\0')
 		{
 			break;
-			s1++;
-			s2++;
+			*s1++;
+			*s2++;
 			count++;
 		}
 	}
 
-	if (s1 == '\0' && s2 == '\0')
+	if (*s1 == '\0' && *s2 == '\0')
 		return (0);
 	else
 		return (count);
