2d872d4a941097368999e3a31e700dc207176750
akadwa <kadazh123@gmail.com>
2022-10-03 03:24:49 -0700

    Added file 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the program's name
+ *
+ * @argc: Argument counter
+ * @argv: Argument vector
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef ALX_HEADER_FILE
+#define ALX_HEADER_FILE
+
+int main (int argc, char *argv[]);
+
+#endif
