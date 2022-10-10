8d64ebde6783015e7b3e92474bfa61105b5cbb42
akadwa <kadazh123@gmail.com>
2022-10-10 03:06:24 -0700

    Added file 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled from
+ *
+ * Return: 0 on SUCCESS
+ */
+
+int main()
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
