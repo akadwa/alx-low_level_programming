24766038b5ad1812ffe752a3072bd91ed70e51c9
akadwa <kadazh123@gmail.com>
2022-10-10 09:25:08 -0700

    Added dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * stuct dog - Includes details on dog
+ *
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
