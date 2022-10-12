b4b5e994db4c90488094e29d088a7e5ebfdd4e64
akadwa <kadazh123@gmail.com>
2022-10-12 10:41:17 -0700

    Added file 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name
+ *
+ * @name: String containing the name
+ * @f: Pointer to function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		(f) (name);
+	}
+}
