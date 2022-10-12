5673616d2de62bd86fb1c0e22a9874f74636f62c
akadwa <kadazh123@gmail.com>
2022-10-12 12:32:26 -0700

    Added files for task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Addition operation
+ *
+ * @a: First term
+ * @b: Second term
+ *
+ * Return: Result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtraction operation
+ *
+ * @a: First term
+ * @b: Second term
+ *
+ * Return: Result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplication operation
+ *
+ * @a: First term
+ * @b: Second  term
+ *
+ * Return: Result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Division operation
+ *
+ * @a: First term
+ * @b: Second term
+ *
+ * Return: Result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modulo operation
+ *
+ * @a: First term
+ * @b: Second term
+ *
+ * Return: Result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
